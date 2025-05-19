9a153ff5827fc73e9d022f5fb9029477f6935c28
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-05-19 16:10:16 -0400

    Add computerChoice function

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let randNum = Math.random();
+
+    if(randNum < 0.3) {
+        return 'Rock';
+    } else if (randNum < 0.6) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
