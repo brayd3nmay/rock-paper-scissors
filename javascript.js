aca1d3bb61f764ce7f8b655300ab3faf7baf7e28
Brayden May <127856333+brayd3nmay@users.noreply.github.com>
2025-05-19 16:27:00 -0400

    Add round playing logic

--- javascript.js
+++ javascript.js
@@ -18,5 +18,48 @@ let computerScore = 0;
 let humanScore = 0;
 
 function playRound(computer, human) {
-    if(computer)
+    if(computer == human)
+    {
+        console.log('Round Draw');
+    }
+    else if(computer == 'ROCK' && human == 'SCISSORS')
+    {
+        console.log('You loose. Rock beats scissors.');
+
+        computerScore++;
+    }
+    else if(computer == 'SCISSORS' && human == 'PAPER')
+    {
+        console.log('You loose. Scissors beats paper.');
+
+        computerScore++;
+    }
+    else if(computer == 'PAPER' && human == 'ROCK')
+    {
+        console.log('You loose. Paper beats rock.');
+
+        computerScore++;
+    }
+    else if(computer == 'SCISSORS' && human == 'ROCK')
+    {
+        console.log('You win. Rock beats scissors.');
+
+        humanScore++;
+    }
+    else if(computer == 'PAPER' && human == 'SCISSORS')
+    {
+        console.log('You win. Scissors beats paper.');
+
+        humanScore++;
+    }
+    else if(computer == 'ROCK' && human == 'PAPER')
+    {
+        console.log('You win. Paper beats rock.');
+
+        humanScore++;
+    }
+    else
+    {
+        console.log('Error');
+    }
 }
